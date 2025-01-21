e3588e4db94aeae85ca8462917b1d0dc9d4c06c1
eunseo <92281453+les0498@users.noreply.github.com>
2025-01-21 17:12:15 +0900

    TodoList UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react' 
+import Header from './components/Header'
+import Editor from './components/Editor'
+import List from './components/List'
+
+const mockData = [
+  {
+    id: 0, 
+    isDoen: false,
+    content : "React 공부하기",
+    date : new Date().getTime(),
+  },
+  {
+    id: 1, 
+    isDoen: false,
+    content : "React 빨래하기",
+    date : new Date().getTime(),
+  },
+  {
+    id: 2, 
+    isDoen: false,
+    content : "React 노래 연습하기",
+    date : new Date().getTime(),
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todos, setTodos] = useState(mockData);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+    <Header />
+    <Editor />
+    <List /> 
+    </div>
   )
 }
 
